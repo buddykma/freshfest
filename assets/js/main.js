7a457fb9af9b85b99d9f429bd4f062bd171431ab
kirill-kundik <goliathusua@gmail.com>
2018-08-28 12:32:08 +0300

    i'm done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,93 @@
-let marker = null;
+let markers = [{
+    name: 'challenge',
+    position: [50.46601315549836 , 30.522695403841],
+    title: 'Зона Челенджів',
+    content: '«Кожен учень справжнього Гоґвортсу мріє взяти участь у Тричаклунському турнірі. А що, якщо жереб випаде на тебе? Ти приймаєш челендж?»\n' +
+        '\n' +
+        'Зона челенжів – це зона для тих, кому «не слабо» і хто справді готовий відповісти на виклики. Челенджі будуть le;t різними, і ось лише декілька з них: зробити селфі із 50 фрешами; знайти людину із твоїм кольором очей та взяти у неї інтерв\'ю; назвати 5 історичних фактів про Могилянку тощо'
+}, {
+    name: 'charity',
+    position: [50.46600897243394 , 30.52285968914316],
+    title: 'Черіті локація',
+    content: 'Могилянка – це завжди про взаємогдопомогу й добро. Тому в нас на фесті є благодійна локація, де можна залишити прочитану книгу на добру справу, а саме у культурний простір на підконтрольній Україні чистині Донбасу.'
+}, {
+    name: 'cyber',
+    position: [50.46644090792224 , 30.522363781929016],
+    title: 'Кіберзона',
+    content: 'Тут можна зіграти у відеоігри, почілити на пуфах із приставкою в руках, знайти однодумців та позмагатися з ними.'
+}, {
+    name: 'fotosushka',
+    position: [50.46609681420831 , 30.522623117947887],
+    title: 'Фотосушка',
+    content: 'Локація, де можна залишити свої фото і роздивитись чужі. Впевнені, у тебе вдома є знімки, які ти з хвилюванням в серці дістаєш з  далеких шухляд, щоби знову повернутися в той щасливий момент. Тепер ти зможеш поділитись ними з усіма могилянцями! Тут ти зможеш розмістити їх за допомогою прищіпки на мотузці, а також подивитися, на теплі спогади інших або навіть забрати частинку цієї атмосфери з собою! Не забудь залишити свій контакт або Інстаграм, коли принесеш знімок, та не соромся забирати інші знімки собі на пам\'ять!'
+}, {
+    name: 'geek',
+    position: [50.46613864350775 , 30.522774260524102],
+    title: 'Гік-локація',
+    content: 'Любителі аніме, фентезі та комп’ютерних ігор знайдуть тут свій рай. І куди ж без тематичних конкурсів і вікторин?'
+}, {
+    name: 'kvartyrnyk',
+    position: [50.46644920901951 , 30.52209740498097],
+    title: 'Квартирник',
+    content: 'У Могилянці  без квартирників нікуди, і ФрешФест не є винятком. Тут будуть атмосферні співи наприкінці фесту, де кожен охочий фреш і нефреш може продемонструвати свій музичний талант, послухати інших й отримати, можливо, найперші у своєму житті оплески. Фішка цьогорічного квартирнику – "битва стилів", а це означає, що жанрових обмежень немає й кожен має шанс почути саме свою улюблену пісню. Початок планується на 17:30'
+}, {
+    name: 'lecture',
+    position: [50.465741263668896 , 30.522879403150228],
+    title: 'Лекторій',
+    content: 'Це 5 лекцій на різну тематику від кльових спікерів, які допоможуть тобі з першого ж дня пройнятися могилянським духом і дізнатися щось корисне.'
+}, {
+    name: 'letters',
+    position: [50.4659085819028 , 30.52254426101922],
+    title: 'Листи в майбутнє',
+    content: 'Пишеш листа собі ж на 4 роки старшому (хіба не магія?), вкидуєш в одну з коробок, чекаєш і в далекому 2022 році зі сльозами на очах перечитуєш його на Конвокації.'
+}, {
+    name: 'lit',
+    position: [50.46590439882912 , 30.523464258875038],
+    title: 'Літературні посиденькі',
+    content: 'Ще один невід\'ємний атрибут могилянського життя, що включає в себе читання, декламування й розмови про високе. Таких посиденьок буде навіть у кількох форматах:  у «Білому просторі» вживу, а також написані або надруковані твори чи вірші, що будуть розвішені, як на фотосушці (чи-то творосушці).\n' +
+        'Заздалегідь зареєстровані учасники й учасниці матимуть змогу отримати призи!'
+}, {
+    name: 'lounge',
+    position: [50.46564505545803 , 30.522682260787633],
+    title: 'Лаундж зона',
+    content: 'Релакс на пуфах, можливість поговорити, відпочити від метушні й зіграти в настільні ігри.'
+}, {
+    name: 'memes',
+    position: [50.46638961852754 , 30.52244569016534],
+    title: 'Локація мемів',
+    content: 'Тут можна посміятися від душі. Локація складається з 2 частин. На першій ти матимеш змогу перевірити свої знання найпопулярніших мемів і отримати стікер в подарунок. А от на другій (тільки тут і тільки у нас!) ти матимеш змогу стати дипломованим "PhD in Memetics", пройшовши спеціальний мемологічний тест. Саме так, тобі не доведеться чекати аж 4 роки до випуску, і вже з першого уніерситетського дня ти матимеш на чому різати ковбасу або чим шпаклювати стіни.'
+}, {
+    name: 'photozone',
+    position: [50.465808191158565 , 30.522511404122497],
+    title: 'Фотозона',
+    content: '3 точки, які ти знайдеш на мапі фесту, де можна зробити кайфові фотки на память про твій гоґвартівський ФРЕШфест.'
+}, {
+    name: 'so',
+    position: [50.46596714301975 , 30.522057976557562],
+    title: 'Студентські організації',
+    content: '3 точки, які ти знайдеш на мапі фесту, де можна зробити кайфові фотки на память про твій гоґвартівський ФРЕШфест.'
+}, {
+    name: 'speeddating',
+    position: [50.414379, 30.579131],
+    title: 'Cпіддейтинг',
+    content: '3 точки, які ти знайдеш на мапі фесту, де можна зробити кайфові фотки на память про твій гоґвартівський ФРЕШфест.'
+}, {
+    name: 'sport',
+    position: [50.46640737999476 , 30.521880548447598],
+    title: 'Спортивна зона',
+    content: 'Для любителів активного способу життя та вправляння у спортивних вміннях. На тебе чекає волейбол, настільний теніс і навіть шахи (для найвитриваліших спортсменів). Крім того, аби втихомирити свій азарт, буже можливість взяти участь в змаганнях на «найміцнішу біцуху», «найспортивніший лікоть», зіграти в гру «Переправа» на швидкість з іншими командами та інше. Може, ти ще вмієш на одній нозі, із закритими очима та з стаканом води на голові стояти? Покажи, на що ти здатен, і спробуй отримати за це винагороду'
+}];
+
+let infowindows = [];
 
 function initMap() {
-    center = new google.maps.LatLng(50.464379, 30.519131);
+    center = new google.maps.LatLng(50.46613944179753, 30.522331113700375);
 
     var html_element = document.getElementById("google_map");
 
     var mapProp = {
         center: center,
-        zoom: 15,
+        zoom: 19,
         disableDefaultUI: true,
         styles: [
             {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
@@ -93,34 +173,76 @@ function initMap() {
 
     var map = new google.maps.Map(html_element, mapProp);
 
-    var contentString = '<div style="color: black; text-align: left"><div style="font-size: 22px;border-bottom: 1px solid gray; margin-bottom: 5px"><b>Title</b></div><div>that was easy</div></div>';
-
-    var infowindow = new google.maps.InfoWindow({
-        content: contentString
+    google.maps.event.addListener(map, 'click', function( event ){
+        console.log( "Latitude: "+event.latLng.lat()+" "+", longitude: "+event.latLng.lng() );
     });
 
-    google.maps.event.addListener(infowindow,'closeclick',function(){
-        $('.not-active').css('visibility','visible').hide().fadeIn(1000);
-        $('.holder').removeClass('not-active').addClass('not-active');
-        // alert('closed') //removes the marker
-        // then, remove the infowindows name from the array
-    });
+    initMarkers(map);
+}
 
-    marker = new google.maps.Marker({
-        position: center,
-        map: map,
-        icon: "assets/images/map-icon.png",
-        title: 'Marker'
-    });
+function initMarkers(map) {
 
-    marker.addListener('click', function () {
-        infowindow.open(map, marker);
+    markers.forEach(function (item) {
+        var contentString = '<div style="color: black; max-width: 80%; max-height: 60%; font-family: Trebuchet MS,serif; text-align: left"><div style="font-size: 22px;border-bottom: 1px solid gray; margin-bottom: 5px"><b>' + item.title + '</b></div><div>' + item.content + '</div></div>';
+
+        var infowindow = new google.maps.InfoWindow({
+            content: contentString
+        });
+
+        google.maps.event.addListener(infowindow, 'closeclick', function () {
+            $('.not-active').css('visibility', 'visible').hide().fadeIn(1000);
+            $('.holder').removeClass('not-active').addClass('not-active');
+            // alert('closed') //removes the marker
+            // then, remove the infowindows name from the array
+            marker.setAnimation(null);
+        });
+
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(item.position[0], item.position[1]),
+            map: map,
+            icon: "assets/images/map-icon.png",
+            title: item.title,
+            animation: google.maps.Animation.DROP
+        });
+
+        marker.addListener('click', function () {
+            infowindows.forEach(function (item) {
+               item.close();
+            });
+            infowindow.open(map, marker);
+            buttonClick(item.name);
+
+            if (marker.getAnimation() !== null) {
+                marker.setAnimation(null);
+            } else {
+                marker.setAnimation(google.maps.Animation.BOUNCE);
+            }
+        });
+
+        item.marker = marker;
+        item.infowindow = infowindow;
+        infowindows.push(infowindow);
     });
 }
-;
 
-function markerClick() {
-    google.maps.event.trigger(marker, 'click');
+function markerClick(val) {
+    markers.forEach(function (item) {
+        if (item.name === val) {
+            google.maps.event.trigger(item.marker, 'click');
+        }
+    })
+}
+
+function buttonClick(val) {
+
+    $('.holder').each(function () {
+        if ($(this).find('input').val() === val)
+            $(this).removeClass('not-active');
+    });
+
+    $('.not-active').fadeOut(1000, function () {
+        $('.not-active').css({"display": "block", "visibility": "hidden"});  // <-- Style Overwrite
+    });
 }
 
 $(window).load(function () {
@@ -149,10 +271,7 @@ $(function () {
 
     $('.holder').click(function () {
         // console.log('clicked')
-        $(this).removeClass('not-active');
-        $('.not-active').fadeOut(1000, function() {
-            $('.not-active').css({"display": "block","visibility": "hidden"});  // <-- Style Overwrite
-        });
-        markerClick();
+
+        markerClick($(this).find('input').val());
     });
 });
